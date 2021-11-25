3b96db70a09f863da7bfaa2d68c24a38c3111b44
AlfredoEsposito <espositoalf31@gmail.com>
2021-11-25 12:41:26 +0100

    Added vets list and updated vetsIndex. Closes #14

--- petClinic-project-web/src/main/java/alten/alfredo/petclinicproject/controllers/VeterinariController.java
+++ petClinic-project-web/src/main/java/alten/alfredo/petclinicproject/controllers/VeterinariController.java
@@ -1,13 +1,22 @@
 package alten.alfredo.petclinicproject.controllers;
 
+import alten.alfredo.petclinicproject.services.VeterinarioService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VeterinariController {
 
+    private final VeterinarioService veterinarioService;
+
+    public VeterinariController(VeterinarioService veterinarioService) {
+        this.veterinarioService = veterinarioService;
+    }
+
     @RequestMapping({"/vetsIndex", "/vetsIndex.html"})
-    public String veterinari(){
+    public String veterinari(Model model){
+        model.addAttribute("veterinari", veterinarioService.findAll());
         return "vetsIndex";
     }
 }
