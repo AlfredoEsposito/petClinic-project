b73105730805e7a7b36f43b26337bf4b6129ba95
AlfredoEsposito <espositoalf31@gmail.com>
2021-12-03 12:29:06 +0100

    added spring data jpa service for vets. closes #42

--- petClinic-project-data/src/main/java/alten/alfredo/petclinicproject/repositories/VeterinarioRepository.java
+++ petClinic-project-data/src/main/java/alten/alfredo/petclinicproject/repositories/VeterinarioRepository.java
@@ -4,4 +4,6 @@ import alten.alfredo.petclinicproject.model.Veterinario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VeterinarioRepository extends CrudRepository<Veterinario, Long> {
+
+    Veterinario findByCognome(String cognome);
 }
---
+++ petClinic-project-data/src/main/java/alten/alfredo/petclinicproject/services/springdatajpaservices/VeterinarioSDJpaService.java
@@ -0,0 +1,60 @@
+package alten.alfredo.petclinicproject.services.springdatajpaservices;
+
+import alten.alfredo.petclinicproject.model.Veterinario;
+import alten.alfredo.petclinicproject.repositories.VeterinarioRepository;
+import alten.alfredo.petclinicproject.services.VeterinarioService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VeterinarioSDJpaService implements VeterinarioService {
+
+    private final VeterinarioRepository veterinarioRepository;
+
+    public VeterinarioSDJpaService(VeterinarioRepository veterinarioRepository) {
+        this.veterinarioRepository = veterinarioRepository;
+    }
+
+    @Override
+    public Veterinario findById(Long aLong) {
+       /* Optional<Veterinario> veterinarioOptional = veterinarioRepository.findById(aLong);
+        if(veterinarioOptional.isPresent()){
+            return veterinarioOptional.get();
+        }else{
+            return null;
+        } */
+
+        return veterinarioRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Set<Veterinario> findAll() {
+        Set<Veterinario> veterinari = new HashSet<>();
+        veterinarioRepository.findAll().forEach(veterinari::add);
+        return veterinari;
+    }
+
+    @Override
+    public Veterinario save(Veterinario object) {
+        return veterinarioRepository.save(object);
+    }
+
+    @Override
+    public void delete(Veterinario object) {
+        veterinarioRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        veterinarioRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Veterinario findByCognome(String cognome) {
+        return veterinarioRepository.findByCognome(cognome);
+    }
+}
